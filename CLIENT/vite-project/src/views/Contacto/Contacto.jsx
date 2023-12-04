df3175b9e69bb6296bb65cf70e975d67c39b4d62
ricardo <rblazquez111@gmail.com>
2023-12-04 02:12:34 -0300

    Fix contacto

--- CLIENT/vite-project/src/views/Contacto/Contacto.jsx
+++ CLIENT/vite-project/src/views/Contacto/Contacto.jsx
@@ -1,99 +1,56 @@
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+
 export default function Contacto() {
 
     return (
-        <div className="About w-96 h-96 relative bg-white">
-
-            <div className="absolute flex-col justify-start items-start gap-10 inline-flex">
-                <div className="OurStory text-justify text-black text-5xl font-semibold font-['Inter'] leading-10 tracking-widest">Our Story</div>
-                <div className="Frame868 flex-col justify-start items-start gap-6 flex">
-                    <div className="LauncedIn2015ExclusiveIsSouthAsiaSPremierOnlineShoppingMakterplaceWithAnActivePresenseInBangladeshSupportedByWideRangeOfTailoredMarketingDataAndServiceSolutionsExclusiveHas10500SallersAnd300BrandsAndServes3MillioonsCustomersAcrossTheRegion w-96 text-black text-base font-normal font-['Poppins'] leading-relaxed">Launced in 2015, Exclusive is South Asia’s premier online shopping makterplace with an active presense in Bangladesh. Supported by wide range of tailored marketing, data and service solutions, Exclusive has 10,500 sallers and 300 brands and serves 3 millioons customers across the region. </div>
-                    <div className="ExclusiveHasMoreThan1MillionProductsToOfferGrowingAtAVeryFastExclusiveOffersADiverseAssotmentInCategoriesRangingFromConsumer w-96 text-black text-base font-normal font-['Poppins'] leading-relaxed">Exclusive has more than 1 Million products to offer, growing at a very fast. Exclusive offers a diverse assotment in categories ranging  from consumer.</div>
-                </div>
-            </div>
-            <div className="SideImage h-96 left-[735px] top-[285px] absolute bg-pink-400 rounded-tl rounded-bl justify-end items-center inline-flex">
-                <img className="PortraitTwoAfricanFemalesHoldingShoppingBagsWhileReactingSomethingTheirSmartphone1 w-96 h-96" src="https://via.placeholder.com/837x609" />
-            </div>
+        < >
+            <div className="w-full h-full flex flex-row-reverse justify-center flex-wrap bg-white" >
 
 
-            <div className="Frame890 left-[135px] top-[1404px] absolute justify-start items-start gap-7 inline-flex">
-                <div className="Frame888 flex-col justify-start items-start gap-8 inline-flex">
-                    <div className="Frame874 h-96 px-16 pt-10 bg-neutral-100 rounded flex-col justify-end items-center flex">
-                        <img className="Image46 w-60 h-96" src="https://via.placeholder.com/236x391" />
-                    </div>
-                    <div className="Frame884 flex-col justify-start items-start gap-4 flex">
-                        <div className="Frame878 flex-col justify-start items-start gap-2 flex">
-                            <div className="TomCruise text-black text-3xl font-medium font-['Inter'] leading-loose tracking-wider">Tom Cruise</div>
-                            <div className="FounderChairman text-black text-base font-normal font-['Poppins'] leading-normal">Founder & Chairman</div>
-                        </div>
-                        <div className="Frame877 justify-start items-start gap-4 inline-flex">
-                            <div className="IconTwitter w-6 h-6 pr-0.5 justify-start items-center flex">
-                                <div className="Group w-7 h-6 relative">
-                                </div>
-                            </div>
-                            <div className="IconInstagram w-6 h-6 p-0.5 justify-center items-center flex">
-                                <div className="Group w-4 h-4 relative">
-                                </div>
-                            </div>
-                            <div className="IconLinkedin w-6 h-6 relative" />
+
+                <div className="px-8 py-10 bg-white rounded shadow justify-center items-center inline-flex">
+                    <div className=" self-stretch flex-col justify-start items-end gap-8 inline-flex">
+                        <textarea name="message" cols="30" rows="10" className=' bg-gray-300'></textarea>
+
+                        <div className="Button px-12 py-4 bg-red-500 rounded justify-center items-center gap-2.5 inline-flex">
+                            <div className="VerTodosLosProductos text-neutral-50 text-base font-medium font-['Poppins'] leading-normal">Send Massage</div>
                         </div>
                     </div>
                 </div>
-                <div className="Frame887 flex-col justify-start items-start gap-8 inline-flex">
-                    <div className="Frame875 h-96 px-9 pt-8 bg-neutral-100 rounded flex-col justify-end items-center flex">
-                        <img className="Image51 w-72 h-96" src="https://via.placeholder.com/294x397" />
-                    </div>
-                    <div className="Frame885 flex-col justify-start items-start gap-4 flex">
-                        <div className="Frame880 flex-col justify-start items-start gap-2 flex">
-                            <div className="EmmaWatson text-black text-3xl font-medium font-['Inter'] leading-loose tracking-wider">Emma Watson</div>
-                            <div className="ManagingDirector text-black text-base font-normal font-['Poppins'] leading-normal">Managing Director</div>
+                <div className="px-9 pt-10 pb-12 bg-white rounded shadow flex-col justify-around items-center inline-flex">
+                    <div className="self-stretch flex flex-col justify-evenly items-center gap-8">
+                        <div className="flex-col justify-start items-start gap-6 flex">
+                            <div className="justify-start items-center gap-4 inline-flex">
+                                <div className="IconsPhone w-10 h-10 flex items-center justify-center bg-red-500 rounded-3xl">
+                                    <LocalPhoneIcon />
+                                </div>
+                                <div className="CallToUs text-black text-base font-medium font-['Poppins'] leading-normal">Llamanos</div>
+                            </div>
+                            <div className="Frame859 flex-col justify-start items-start gap-4 flex">
+                                <div className="WeAreAvailable2477DaysAWeek w-64 text-black text-sm font-normal font-['Poppins'] leading-tight">Nosotros estamos disponible 24/7, 7 dias a la semana.</div>
+                                <div className="Phone8801611112222 text-black text-sm font-normal font-['Poppins'] leading-tight">Phone: +8801611112222</div>
+                            </div>
                         </div>
-                        <div className="Frame879 justify-start items-start gap-4 inline-flex">
-                            <div className="IconTwitter w-6 h-6 pr-0.5 justify-start items-center flex">
-                                <div className="Group w-7 h-6 relative">
-                                </div>
-                            </div>
-                            <div className="IconInstagram w-6 h-6 p-0.5 justify-center items-center flex">
-                                <div className="Group w-4 h-4 relative">
-                                </div>
-                            </div>
-                            <div className="IconLinkedin w-6 h-6 relative" />
+                        <div className="Underline w-64 h-px opacity-50 justify-center items-center inline-flex">
+                            <div className="Line1 w-64 h-px border border-black"></div>
                         </div>
-                    </div>
-                </div>
-                <div className="Frame889 flex-col justify-start items-start gap-8 inline-flex">
-                    <div className="Frame876 h-96 px-5 pt-9 bg-neutral-100 rounded flex-col justify-end items-center flex">
-                        <img className="Image47 w-80 h-96" src="https://via.placeholder.com/326x392" />
-                    </div>
-                    <div className="Frame886 flex-col justify-start items-start gap-4 flex">
-                        <div className="Frame882 flex-col justify-start items-start gap-2 flex">
-                            <div className="WillSmith text-black text-3xl font-medium font-['Inter'] leading-loose tracking-wider">Will Smith</div>
-                            <div className="ProductDesigner text-black text-base font-normal font-['Poppins'] leading-normal">Product Designer</div>
-                        </div>
-                        <div className="Frame881 justify-start items-start gap-4 inline-flex">
-                            <div className="IconTwitter w-6 h-6 pr-0.5 justify-start items-center flex">
-                                <div className="Group w-7 h-6 relative">
+                        <div className="Frame862 flex-col justify-start items-start gap-6 flex">
+                            <div className="Frame857 justify-start items-center gap-4 inline-flex">
+                                <div className="IconsMail w-10 h-10 flex items-center justify-center bg-red-500 rounded-3xl">
+                                    <MailOutlineIcon />
                                 </div>
+                                <div className=" text-black text-base font-medium font-['Poppins'] leading-normal">Escribenos un correo</div>
                             </div>
-                            <div className="IconInstagram w-6 h-6 p-0.5 justify-center items-center flex">
-                                <div className="Group w-4 h-4 relative">
-                                </div>
+                            <div className="flex-col justify-start items-start gap-4 flex">
+                                <span>Emails: customer@exclusive.com</span>
                             </div>
-                            <div className="IconLinkedin w-6 h-6 relative" />
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="Frame883 left-[664px] top-[2008px] absolute justify-start items-center gap-3 inline-flex">
-                <div className="Ellipse7 w-3 h-3 opacity-30 bg-black rounded-full" />
-                <div className="Ellipse2 w-3 h-3 opacity-30 bg-black rounded-full" />
-                <div className="Group1000005940 w-4 h-4 relative">
-                    <div className="Ellipse4 w-2.5 h-2.5 left-[3px] top-[3px] absolute bg-red-500 rounded-full" />
-                    <div className="Ellipse5 w-4 h-4 left-0 top-0 absolute opacity-30 rounded-full border-2 border-black" />
-                </div>
-                <div className="Ellipse3 w-3 h-3 opacity-30 bg-black rounded-full" />
-                <div className="Ellipse6 w-3 h-3 opacity-30 bg-black rounded-full" />
+
             </div>
 
-        </div>
+        </ >
     )
 }
